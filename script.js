7552d41984eee6468d3b26f795fc189c6fed9711
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2023-04-27 16:02:20 +0500

    feat: create keyboard img

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ const keyboardKeys = [
   [',', 'б'],
   ['.', 'ю'],
   ['/', '.'],
-  ['&#8593;', '&#8593;'],
+  ['&#9650;', '&#9650;'],
   ['Shift', 'Shift'],
 
   ['Ctrl', 'Ctrl'],
@@ -85,14 +85,67 @@ const keyboard = document.createElement('div');
 keyboard.className = 'keyboard';
 textarea.after(keyboard);
 
+const row1 = document.createElement('div');
+row1.className = 'row1';
+keyboard.prepend(row1);
+
+const row2 = document.createElement('div');
+row1.className = 'row2';
+row1.after(row2);
+
+const row3 = document.createElement('div');
+row1.className = 'row3';
+row2.after(row3);
+
+const row4 = document.createElement('div');
+row1.className = 'row4';
+row3.after(row4);
+
+const row5 = document.createElement('div');
+row1.className = 'row5';
+row4.after(row5);
+
 const footer = document.createElement('footer');
 footer.className = 'footer';
 footer.innerHTML = 'Клавиатура создана в операционной системе Windows<br>Для переключения языка комбинация: левые ctrl + alt';
 document.body.append(footer);
 
-
 function createKeyboard() {
-    for (let i=0; i<keyboardKeys.length; i+=1) {
-        
-    }
+  for (let i = 0; i < 14; i += 1) {
+    const key = document.createElement('span');
+    row1.append(key);
+    row1.classList = 'row';
+    key.className = 'key';
+    key.innerHTML = keyboardKeys[i][0];
+  }
+  for (let i = 14; i < 29; i += 1) {
+    const key = document.createElement('span');
+    row2.append(key);
+    row2.classList = 'row';
+    key.className = 'key';
+    key.innerHTML = keyboardKeys[i][0];
+  }
+  for (let i = 29; i < 42; i += 1) {
+    const key = document.createElement('span');
+    row3.append(key);
+    row3.classList = 'row';
+    key.className = 'key';
+    key.innerHTML = keyboardKeys[i][0];
+  }
+  for (let i = 42; i < 55; i += 1) {
+    const key = document.createElement('span');
+    row4.append(key);
+    row4.classList = 'row';
+    key.className = 'key';
+    key.innerHTML = keyboardKeys[i][0];
+  }
+  for (let i = 55; i < 64; i += 1) {
+    const key = document.createElement('span');
+    row5.append(key);
+    row5.classList = 'row';
+    key.className = 'key';
+    key.innerHTML = keyboardKeys[i][0];
+  }
 }
+
+createKeyboard();
